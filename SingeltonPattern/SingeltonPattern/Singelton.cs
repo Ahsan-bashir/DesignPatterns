294623323c9369b84d5a37dfc7c27e18fe01fbb4
Ahsan-Bashir <bteeccc@gmail.com>
2024-03-17 08:15:17 +0500

    comments updated

--- SingeltonPattern/SingeltonPattern/Singelton.cs
+++ SingeltonPattern/SingeltonPattern/Singelton.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SingeltonPattern
@@ -9,20 +6,20 @@ namespace SingeltonPattern
     class Singelton
     {
         private static Singelton instance;  // static is to ensure only 1 instance
-        private static readonly object s1 = new object();   // to implement Sunchronization
-        private Singelton()
+        private static readonly object s1 = new object(); // lock object holded by 1 thread at a time ==> to implement Sunchronization
+        private Singelton() // private keyword is to discourage multiple instance outside of class
         {
             Console.WriteLine("Instance Created !!!");
         }
         public static Singelton CreateInstance()
-        {   //  Double Lock
+        {   //  Double Lock ==> solution of performance issue
             if (instance == null)
             {
                 //  Single Lock
-                lock (s1)   //  synchronization ==> decread=se performance 100 times
+                lock (s1)   //  synchronization ==> decrease performance 100 times but ensures thread-safety
                 {
                     if (instance == null) { return instance = new Singelton(); }
-                    else { Console.WriteLine("Don't try to create multiple times !!!"); }
+                    else { Console.WriteLine("Don't try to create instance multiple times !!!"); }
                 }
             }
                 return instance;
